c821cd4f816c108617dd72755f34b0e53220f95b
Doebelnatz <axel.ej.adlercreutz@gmail.com>
2020-07-26 13:44:38 +0300

    update

--- client/src/App/Apply/ApplyFormSection/index.tsx
+++ client/src/App/Apply/ApplyFormSection/index.tsx
@@ -1,12 +1,39 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useRef, useState } from 'react';
 import { ApplyForm, ApplyFormSectionDiv, FormDiv } from './Styles';
+import { makeRequest } from '../../../Api';
+import { strict } from 'assert';
 
 const ApplyFormSection: React.FC = () => {
-	const [selectedFile, setSelectedFile] = useState(null);
-
-	const fileChangeHandler = (event: any) => {
+	const [selectedFile, setSelectedFile] = useState<File>();
+	const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+	const handleFileChange = (event: any) => {
 		console.log(event.target.files);
 		setSelectedFile(event.target.files[0]);
+	};
+
+	const handleFileUpload = async (e: any) => {
+		e.preventDefault();
+		const data = new FormData();
+		if (!!selectedFile) {
+			data.append('file', selectedFile);
+			try {
+				await makeRequest('/applications/upload-file', 'POST', data);
+				setUploadedFiles([...uploadedFiles, selectedFile]);
+				console.log(uploadedFiles);
+			} catch (e) {}
+		}
+	};
+
+	const renderUploadedFiles = () => {
+		console.log(uploadedFiles);
+		return uploadedFiles.map((file: any, index) => {
+			console.log('files:');
+			return (
+				<div key={index}>
+					<p>{file.name}</p>
+				</div>
+			);
+		});
 	};
 
 	return (
@@ -31,8 +58,10 @@ const ApplyFormSection: React.FC = () => {
 					</label>
 					<label>
 						Attached files
-						<input type={'file'} onChange={fileChangeHandler} />
+						<input type={'file'} onChange={handleFileChange} />
 					</label>
+					{renderUploadedFiles()}
+					<button onClick={handleFileUpload}>UPLOAD</button>
 					<button type={'submit'}>SUBMIT</button>
 				</ApplyForm>
 			</FormDiv>
