c4b808aaf9a0a16714a3df6518be34ea4917fe52
doebelnatz <axel.ej.adlercreutz@gmail.com>
2020-08-24 15:05:32 +0300

    added cogwheel icon

--- client/src/App/Apply/index.tsx
+++ client/src/App/Apply/index.tsx
@@ -3,12 +3,14 @@ import NavBar from '../NavBar';
 import HomeImg from '../Home/HomeImg';
 import { ApplyDiv } from './Styles';
 import ApplyFormSection from './ApplyFormSection';
+import Footer from "../Footer";
 
 const Apply: React.FC = () => {
 	return (
 		<ApplyDiv>
 			<HomeImg />
 			<ApplyFormSection />
+			<Footer/>
 		</ApplyDiv>
 	);
 };
